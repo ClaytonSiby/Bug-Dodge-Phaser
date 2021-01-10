1dfe3493141c9290e7b4cac97592a6efb7f24845
Clayton Siby <claytonsiby@gmail.com>
2021-01-10 11:06:46 +0200

    Setup & configure basic webpack dev env

---
+++ src/modules/Preload.js
@@ -0,0 +1,30 @@
+const LoadAssetsAndCreate = (() => {
+  const preload = () => {
+    this.load.image(
+      'bug1',
+      'https://content.codecademy.com/courses/learn-phaser/physics/bug_1.png'
+    )
+    this.load.image(
+      'bug2',
+      'https://content.codecademy.com/courses/learn-phaser/physics/bug_2.png'
+    )
+    this.load.image(
+      'bug3',
+      'https://content.codecademy.com/courses/learn-phaser/physics/bug_3.png'
+    )
+    this.load.image(
+      'platform',
+      'https://content.codecademy.com/courses/learn-phaser/physics/platform.png'
+    )
+    this.load.image(
+      'codey',
+      'https://content.codecademy.com/courses/learn-phaser/physics/codey.png'
+    )
+  }
+
+  const create = () => {}
+
+  return { preload, create }
+})()
+
+export default LoadAssetsAndCreate
