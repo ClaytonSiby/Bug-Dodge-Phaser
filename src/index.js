32842573bb2c5987b6ec230f74a1aa3a2fc6aaea
ClaytonSiby <claytonsiby@gmail.com>
2021-01-10 22:49:27 +0200

    Configure eslint & webpack-cli

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import _ from 'lodash';
+const gameState = {};
 
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack', ' ']);
-
-    return element;
+function preload() {
+    this.load.image('bug1', 'https://content.codecademy.com/courses/learn-phaser/physics/bug_1.png');
+    this.load.image('bug2', 'https://content.codecademy.com/courses/learn-phaser/physics/bug_2.png');
+    this.load.image('bug3', 'https://content.codecademy.com/courses/learn-phaser/physics/bug_3.png');
+    this.load.image('platform', 'https://content.codecademy.com/courses/learn-phaser/physics/platform.png');
+    this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/physics/codey.png');
 }
 
-document.body.appendChild(component());
+function create() {
+  this.physics.add.sprite(300, 200, 'codey');
+}
+
+function update() {}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 450,
+    height: 500,
+    backgroundColor: '#b9eaff',
+    physics: {
+        arcade: {
+            gravity: { y: 200 },
+            enableBody: true,
+        }
+    },
+
+    scene: {
+        preload,
+        create,
+        update
+    }
+}
